af6ee3402c2664797ed2c09e5427470ebcac2049
windtronic <122315883+windtronic@users.noreply.github.com>
2023-11-20 14:13:56 -0700

    Update releases.py

--- releases.py
+++ releases.py
@@ -1,15 +1,12 @@
 import requests
 import csv
 
-# GitHub API endpoint to fetch releases for the Bootstrap repository
 api_url = 'https://api.github.com/repos/twbs/bootstrap/releases'
 
-# Function to fetch releases from GitHub API
 def fetch_releases():
     response = requests.get(api_url)
     return response.json()
 
-# Function to write releases to a CSV file
 def write_releases_to_csv(releases):
     with open('bootstrap_releases.csv', 'w', newline='') as csvfile:
         fieldnames = ['Created Date', 'Tag name', 'URL for the distribution zip file']
